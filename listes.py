ab4b642501fc0ca9d27eb73febb8c9392cb75836
KoryahSiha <koryahsiha@gmail.com>
2022-12-13 14:03:01 +0100

    Ajout d'un cours sur les listes
    Création du fichier de cours.
    Ajout de l'accès en lecture d'un élément d'une liste, au nombre d'éléments et à leur index.

---
+++ listes.py
@@ -0,0 +1,34 @@
+fruits = ['ananas', 'banane', 'cerise']
+print(fruits)
+
+# accès en lecture au 0ème élément de la liste
+print(fruits[0])
+
+my_fruit = fruits[0]
+print(my_fruit)
+
+fruits[0] = 'abricot'
+print(fruits)
+print(fruits[0])
+
+my_count = len(fruits)
+
+index = 0
+if index < my_count:
+    print(f'{index =}')
+    print(fruits[index])
+
+index += 1
+if index < my_count:
+    print(f'{index =}')
+    print(fruits[index])
+
+index += 1
+if index < my_count:
+    print(f'{index =}')
+    print(fruits[index])
+
+index += 1
+if index < my_count:
+    print(f'{index =}')
+    print(fruits[index])
