031c1e0658f6ba9328acf38fae35d4825c799d65
KoryahSiha <koryahsiha@gmail.com>
2022-12-14 10:52:53 +0100

    Ajout de l'exercice 5
    Les exercices 05-1 à 05-5 ont été faits.

---
+++ exercices-05/exercice-05-1.py
@@ -0,0 +1,14 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+def multiplication(a: float, b: float) -> float:
+    """Cette fonction permet de multiplier
+
+    a float et b float indiquent un nombre à virgule
+    return un float
+    """
+    return a * b
+    
+help(multiplication)
